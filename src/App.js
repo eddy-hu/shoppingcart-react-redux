058a3d2a524eabb86bbdeed0fbe533e3eb6d76fe
eddy-hu <eddy.hu@yahoo.com>
2019-01-31 11:19:48 -0500

    add redux

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import PickupSavings from './components/PickupSavings/PickupSavings';
 import TaxesFees from './components/TaxesFees/TaxesFees';
 import EstimatedTotal from './components/EstimatedTotal/EstimatedTotal';
 import ItemDetails from './components/ItemDetails/ItemDetails';
+import PromoCodeDiscount from './components/PromoCode/PromoCode';
 
+import { connect } from 'react-redux';
+import { handleChange } from './actions/promoCodeActions';
 
 class App extends Component {
 
@@ -16,9 +19,38 @@ class App extends Component {
       total: 100,
       PickupSavings: -3.8,
       taxes: 0,
-      estimatedTotal: 0 
+      estimatedTotal: 0,
+      disablePromoButton: false
+    };
+  }
+  
+  componentDidMount = () =>{
+    this.setState({
+      taxes: (this.state.total +this.state.PickupSavings) * 0.0875
+    },
+    function() {
+      this.setState({
+        estimatedTotal: this.state.total +this.state.PickupSavings +this.state.taxes
+      });
+    
+    }
+    )
+  }
+
+  giveDiscountHandler = () => {
+    if(this.props.promoCode === 'DISCOUNT'){
+      this.setState({
+        estimatedTotal: this.state.estimatedTotal * 0.9
+      },
+        function(){
+          this.setState({
+            disablePromoButton: true
+          })
+        }
+      )
     }
   }
+ 
   render() {
     return (
       <div className="container">
@@ -28,11 +60,18 @@ class App extends Component {
           <TaxesFees taxes={this.state.taxes.toFixed(2)}></TaxesFees>
           <hr/>
           <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)}></EstimatedTotal>
-          <ItemDetails ></ItemDetails>
+          <ItemDetails price={this.state.estimatedTotal.toFixed(2)}></ItemDetails>
+          <PromoCodeDiscount 
+          giveDiscount={()=> this.giveDiscountHandler()}
+          isDisabled={this.state.disablePromoButton}
+          ></PromoCodeDiscount>
         </Grid>
       </div>
     );
   }
 }
+const mapStateToProps =state => ({
+  promoCode: state.promoCode.value
+})
 
-export default App;
+export default connect(mapStateToProps, {handleChange })(App);
